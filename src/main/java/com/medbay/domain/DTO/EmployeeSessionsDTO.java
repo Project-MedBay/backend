7e53a3adf912eddb8dbcf9bfb7af98bbe4fde8fa
ianba <ian.balen6@gmail.com>
2024-01-11 18:19:06 +0100

    add appointment id for get sessions for employee

--- src/main/java/com/medbay/domain/DTO/EmployeeSessionsDTO.java
+++ src/main/java/com/medbay/domain/DTO/EmployeeSessionsDTO.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class EmployeeSessionsDTO {
 
     private long id;
+    private long appointmentId;
     private String equipmentRoomName;
     private LocalDateTime dateTime;
     private String sessionNotes;
