1c7cc27c5189508fe2eb33ddd183bcd47c428237
ianba <ian.balen6@gmail.com>
2024-01-13 16:42:19 +0100

    fix getting therapy type and add PythonScriptRunner

---
+++ src/main/java/com/medbay/config/PythonScriptRunner.java
@@ -0,0 +1,37 @@
+package com.medbay.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Component
+public class PythonScriptRunner {
+
+    @PostConstruct
+    public void runScript(//String patientId, String input
+                           ) {
+        String patientId = "1";
+        String input = "Boli me miško.";
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python", "src/main/resources/therapyHelper.py", patientId, input);
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
