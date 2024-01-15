c52a6ac841f36241a41ece23b7c94d8f82e1ba22
ianba <ian.balen6@gmail.com>
2024-01-15 03:54:52 +0100

    36 -> 24 hour appointment difference

--- src/main/java/com/medbay/service/AppointmentService.java
+++ src/main/java/com/medbay/service/AppointmentService.java
@@ -162,9 +162,11 @@ public class AppointmentService {
                 boolean overlap = false;
                 for (Appointment patientAppointment : patientAppointments) {
                     if (!patientAppointment.getId().equals(appointment.getId())) {
-                        long hoursDifference = Duration.between(patientAppointment.getDateTime(), dateTime).abs().toHours();
-                        if (hoursDifference <= 24) {
-                            log("Overlap with appointment with dateTime: " + patientAppointment.getDateTime());
+                        LocalDateTime existingAppointmentStart = patientAppointment.getDateTime().minusHours(23);
+                        LocalDateTime existingAppointmentEnd = patientAppointment.getDateTime().plusHours(23);
+                        if ((dateTime.isAfter(existingAppointmentStart) || dateTime.isEqual(existingAppointmentStart)) &&
+                                (dateTime.isBefore(existingAppointmentEnd))) {
+                            log("Overlap within 24-hour period of appointment at: " + patientAppointment.getDateTime());
                             overlap = true;
                             break;
                         }
