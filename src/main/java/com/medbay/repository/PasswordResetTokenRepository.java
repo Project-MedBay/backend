33034eb8bb89e384f9dfc4df07d86ccf95ad6325
ianba <ian.balen6@gmail.com>
2024-01-19 18:00:11 +0100

    fix double email sending of reset password

--- src/main/java/com/medbay/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/medbay/repository/PasswordResetTokenRepository.java
@@ -1,6 +1,7 @@
 package com.medbay.repository;
 
 import com.medbay.domain.PasswordResetToken;
+import com.medbay.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +9,8 @@ import java.util.Optional;
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
 
     Optional<PasswordResetToken> findByToken(String token);
+    
+    boolean existsByUser(User user);
+
+    Optional<PasswordResetToken> findByUser(User user);
 }
