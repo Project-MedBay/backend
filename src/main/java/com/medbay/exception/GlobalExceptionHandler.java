faa217b4c332dc6d0846fde2cb3f796392319d3c
ianba <ian.balen6@gmail.com>
2024-01-19 18:08:02 +0100

    add global exception handler add few unused mbo into health referrals

---
+++ src/main/java/com/medbay/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.medbay.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e){
+        return e.getMessage();
+    }
+
+}
